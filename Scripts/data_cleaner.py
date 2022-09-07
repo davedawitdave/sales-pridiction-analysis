e2c044cbf7db35ca9aee56c7f8fdfe1317faa9b9
Dawit Asmero <76691105+dawitasmeroatinafu@users.noreply.github.com>
2022-09-07 20:10:10 +0300

    data summerzing function is added

---
+++ Scripts/data_cleaner.py
@@ -0,0 +1,12 @@
+from re import I
+from numpy import np
+from pandas import pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+Class Cleaner:
+      
+
+
---
+++ Scripts/plotter_fun.py
@@ -0,0 +1,4 @@
+
+
+Class Plotter:
+      def
