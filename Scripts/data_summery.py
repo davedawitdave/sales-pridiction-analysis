24cc3727a2eeea5d746993d535747a60c8579623
Dawit Asmero <76691105+dawitasmeroatinafu@users.noreply.github.com>
2022-09-08 14:21:21 +0300

    add logging script

--- Scripts/data_summery.py
+++ Scripts/data_summery.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import dvc.api as dvc
@@ -7,25 +7,46 @@ import dvc.api as dvc
 import io 
 import sys
 sys.path.append('../')
+from logger import Logger
+
 
 class Summery:
-    
-    def read_from_file(self,path,low_memory=True):
-        """
-            Load data from a csv file
-        """
-        try:
-            df = pd.read_csv(path)
-            return df
-        except FileNotFoundError:
-            print("File not found.")
-    def read_from_dvc(self,path,repo,rev,low_memory=True):
-        
-        """
-            Load data from a dvc storage
-        """
-        try:
-            data = dvc.api.read(path=path,repo=repo, rev=rev)
-            df = pd.read_csv(io.StringIO(data),low_memory=False)
-        except Exception as e:
-            print("Something went wrong!",e)
+       def __init__(self) -> None:
+            """Initilize class."""
+            try:
+                pass
+                self.logger = Logger("utility.log").get_app_logger()
+                self.logger.info(
+                    'Successfully initialized Object')
+            except Exception:
+                self.logger.exception(
+                    'Failed to initialized Object')
+                sys.exit(1)
+       
+       def read_from_file(self,path,low_memory=True):
+            """
+                Load data from a csv file
+            """
+            try:
+                df = pd.read_csv(path)
+                self.logger.info(f"successsfuly read {path}")
+                return df
+            except FileNotFoundError:
+                self.logger.error(f"failed to read {path}; file not found")
+
+                print("File not found.")
+       def read_from_dvc(self,path,repo,rev,low_memory=True):
+            
+            """
+                Load data from a dvc storage
+            """
+            try:
+                data = dvc.read(path=path,repo=repo, rev=rev)
+                df = pd.read_csv(io.StringIO(data),low_memory=low_memory)
+                self.logger.info(f"successsfuly read {path} from dvc")
+
+                return df
+            except Exception as e:
+                self.logger.error(f"failed to read {path}; {e}")
+
+                print("Something went wrong!",e)
